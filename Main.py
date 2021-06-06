86f2e71b4d70b793669da50fbc2f418ad1b93715
NRS229 <noah229@hotmail.com>
2021-06-06 08:26:00 -0600

    Best practices
    Tidying the code

--- Main.py
+++ Main.py
@@ -4,9 +4,9 @@ import ffmpeg
 from pydub import AudioSegment
 
 
-def translate_structure(structure_path):
+def get_structure_without_groups(structure_path):
     # Variables
-    blocks_path = "Blocks/"
+    blocks_path = "Input/Blocks/"
     folder = ""
     output = ""
     groups = []
@@ -21,7 +21,7 @@ def translate_structure(structure_path):
             folder = line[:-1]
         # Groups section finish
         elif line == ")\n":
-            output += groups[random.randint(0, len(groups)-1)]
+            output += groups[random.randint(0, len(groups) - 1)]
             groups = []
         # Group start
         elif line == "[\n":
@@ -31,95 +31,98 @@ def translate_structure(structure_path):
             in_group = False
             group_number += 1
         # If it's a file
-        elif line.startswith("└ ") or line.startswith("~ "):
-            # If there's a minimum time
-            if "*" in line:
-                pos = line.index("*")
-                min_time = line[pos - 1: pos + 3]
-                line = line.replace(min_time, "")
-            else:
-                min_time = ""
-            # If it repeats (X3)
-            if "X" in line:
-                times = line.split("X")[1]
-                file = line[2:-4]
-            # If it does no repeats
-            else:
-                times = 1
-                file = line[2:-1]
-            # Translate the line the number of times asked
-            for _ in range(int(times)):
-                # If the line is part of a group
-                if in_group:
-                    if len(groups) <= group_number:
-                        groups.append(blocks_path + folder + file + "/" + min_time + "\n")
-                    else:
-                        groups[group_number] += blocks_path + folder + file + "/" + min_time + "\n"
-                # If the line is not part of a group
+        elif line.startswith("└ "):
+            file = line[2:-1]
+            # If the line is part of a group
+            if in_group:
+                if len(groups) <= group_number:
+                    groups.append(blocks_path + folder + file + "\n")
                 else:
-                    # If it is silence (~)
-                    if "~" in line:
-                        output += line
-                    else:
-                        output += blocks_path + folder + file + "/" + min_time + "\n"
+                    groups[group_number] += blocks_path + folder + file + "\n"
+            # If the line is not part of a group
+            else:
+                output += blocks_path + folder + file + "\n"
     # Close the file
     f.close()
     # Return the result as a list
     return output.split("\n")[:-1]
 
 
-def merge_audio_files(input_files, output_name):
+def get_paths_from_structure(structure_array):
+    output = []
+    for line in structure_array:
+        if " " in line:
+            output.append(line.split(" ")[0])
+        else:
+            output.append(line)
+    return output
+
+
+def get_paths_with_random_audio_files(paths_array, repetitions):
+    output = []
+    for i in range(len(paths_array)):
+        for rep in range(repetitions[i]):
+            wav_file = paths_array[i] + "/" + random.choice(os.listdir(paths_array[i]))
+            while (wav_file in output) or (".DS_Store" in wav_file):
+                wav_file = paths_array[i] + "/" + random.choice(os.listdir(paths_array[i]))
+            output.append(wav_file)
+    return output
+
+
+def get_silences_after_audio(structure_array, repetitions):
+    output = []
+    for i in range(len(structure_array)):
+        for rep in range(repetitions[i]):
+            if "~" in structure_array[i]:
+                # silence after audio is between "~" and " "
+                output.append(int((structure_array[i].split("~"))[1].split(" ")[0]))
+            else:
+                output.append(0)
+    return output
+
+
+def get_minimum_times(structure_array, repetitions):
+    output = []
+    for i in range(len(structure_array)):
+        for rep in range(repetitions[i]):
+            if "%" in structure_array[i]:
+                # minimum time is between "%" and " "
+                output.append(int((structure_array[i].split("%"))[1].split(" ")[0]))
+            else:
+                output.append(0)
+    return output
+
+
+def get_repetitions(structure_array):
+    output = []
+    for line in structure_array:
+        if "*" in line:
+            # repetition is between "*" and " "
+            output.append(int((line.split("*"))[1].split(" ")[0]))
+        else:
+            output.append(1)
+    return output
+
+
+def merge_audio_files(output_name, paths, minimum_times, silences):
     output_file = "Output/" + output_name + ".wav"
     output = AudioSegment.empty()
-    for file in input_files:
-        # If there's a silence
-        if "~" in file:
-            # Create silence audio segment
-            time = file.split("~ ")[1]
-            silent_segment = AudioSegment.silent(duration=(1000 * int(time)))
-            output += silent_segment
-        else:
-            # If there's a minimum time
-            if "*" in file:
-                pos = file.index("*")
-                min_time = file[pos - 1: pos + 3]
-                file = file.replace(min_time, "")
-                min_time = min_time.split("*")[1]
-            else:
-                min_time = 0
-            # Read wav file to an audio segment
-            audio = AudioSegment.from_wav(file)
-            # Add silence if the file takes less than the min_time
-            if int(min_time) > audio.duration_seconds:
-                # Create silence audio segment
-                silent_segment = AudioSegment.silent(duration=(1000 * (int(min_time) - audio.duration_seconds)))
-                audio = audio + silent_segment
-            # Add audio to output
-            output += audio
+    for i in range(len(paths)):
+        audio = AudioSegment.from_wav(paths[i])
+        # if there's a minimum time
+        if int(minimum_times[i]) > audio.duration_seconds:
+            # the line was cut because PyCharm asked nicely :)
+            silence_for_minimum_time = \
+                AudioSegment.silent(duration=(1000 * (int(minimum_times[i]) - audio.duration_seconds)))
+            audio += silence_for_minimum_time
+        # if there's a silence after
+            silence_after = AudioSegment.silent(duration=(1000 * (int(silences[i]))))
+            audio += silence_after
+        output += audio
     # Export the final output
     output.export(output_file, format="wav")
 
-
-def create_audio_paths():
-    audio_paths = []
-    for line in translate_structure("Structure.txt"):
-        # If it is silence (~)
-        if "~" in line:
-            audio_paths.append(line)
-        else:
-            # If there's a minimum time
-            if "*" in line:
-                pos = line.index("*")
-                min_time = line[pos - 1: pos + 3]
-                line = line.replace(min_time, "")
-            else:
-                min_time = ""
-            wav_file = line + random.choice(os.listdir(line))
-            while (wav_file in audio_paths) or (".DS_Store" in wav_file):
-                wav_file = line + random.choice(os.listdir(line))
-            else:
-                audio_paths.append(wav_file + min_time)
-    return audio_paths
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
 
 def create_video(image_path, audio_path):
@@ -127,12 +130,24 @@ def create_video(image_path, audio_path):
     input_audio = ffmpeg.input(audio_path)
     (
         ffmpeg
-            .concat(input_still, input_audio, v=1, a=1)
-            .output("Output/output.mp4")
-            .run(overwrite_output=True)
+        .concat(input_still, input_audio, v=1, a=1)
+        .output("Output/output.mp4")
+        .run(overwrite_output=True)
     )
 
 
-# Run the methods
-merge_audio_files(create_audio_paths(), "audio")
-create_video("Input/image.jpg", "Output/audio.wav")
+def main():
+    structure_path = "Structure.txt"
+    structure_without_groups = get_structure_without_groups(structure_path)
+    paths = get_paths_from_structure(structure_without_groups)
+    repetitions = get_repetitions(structure_without_groups)
+    silences_after_audio = get_silences_after_audio(structure_without_groups, repetitions)
+    minimum_times = get_minimum_times(structure_without_groups, repetitions)
+    paths_with_random_audio_files = get_paths_with_random_audio_files(paths, repetitions)
+
+    # export as .wav file
+    merge_audio_files("audio", paths_with_random_audio_files, minimum_times, silences_after_audio)
+
+
+if __name__ == "__main__":
+    main()
